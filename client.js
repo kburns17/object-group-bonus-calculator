e1ea25ed9683de0576eb9be873fc7f2d59e57d10
Kerry Burns <kerryalanburns@gmail.com>
2018-03-07 11:52:18 -0600

    completed bonus calc

--- client.js
+++ client.js
@@ -10,7 +10,7 @@ const employees = [ atticus, jem, scout, robert, mayella ];
 
 console.log(employees);
 
-class Employee {
+/*class Employee {
   constructor(nameIn, employeeNumberIn, annualSalaryIn, reviewRatingIn ){
     this.name = nameIn;
     this.preBonusPercentage = this.preBonusPercentage( reviewRatingIn, employeeNumberIn );
@@ -88,3 +88,91 @@ function updateEmployee( employeeIn ){
   let employeeUpdated = new Employee( employeeIn.name );
   return employeeUpdated;
 }
+*/
+
+
+
+// Walkthrough with Chris
+/*const atticus = { name: 'Atticus', employeeNumber: '2405', annualSalary: '47000', reviewRating: 3 };
+const jem = { name: 'Jem', employeeNumber: '62347', annualSalary: '63500', reviewRating: 4 };
+const scout = { name: 'Scout', employeeNumber: '6243', annualSalary: '74750', reviewRating: 5 };
+const robert = { name: 'Robert', employeeNumber: '26835', annualSalary: '66000', reviewRating: 1 };
+const mayella = { name: 'Mayella', employeeNumber: '89068', annualSalary: '35000', reviewRating: 2 };
+
+const employees = [ atticus, jem, scout, robert, mayella ];*/
+
+class Employee {
+  constructor(nameIn, bonusPercentageIn,totalCompensationIn, totalBonusIn ) {
+    this.name = nameIn;
+    this.bonusPercentage = bonusPercentageIn;
+    this.totalCompensation = totalCompensationIn;
+    this.totalBonus = totalBonusIn;
+  }
+}
+
+
+for (employee of employees) {
+  console.log(employee);
+let name = employee.name;
+let bonusPercentage = baseBonus(employee.reviewRating) +
+                   lengthOfServiceBonus(employee.employeeNumber) +
+                   salaryAdjustment(employee.annualSalary);
+let finalBonus = finalAjustment(bonusPercentage);
+let totalBonus = employee.annualSalary * bonusPercentage;
+let actualBonusInDollars = employee.annualSalary * finalBonus;
+let compensation = employee.annualSalary + totalBonus;
+  const employeeWithBonus = new Employee(name, finalBonus, compensation, actualBonusInDollars );
+}
+
+
+ function finalAjustment(bonusPercentageIn) {
+  if (bonusPercentageIn > 0.13) {
+    return 0.13;
+  } else if (bonusPercentageIn < 0) {
+    return 0;
+  } else {
+    return bonusPercentageIn;
+  }
+}// end finalAjustment
+// console.log(employees);
+// console.log(baseBonus(scout.reviewRating));
+// console.log(lengthOfServiceBonus(scout.employeeNumber));
+// console.log(salaryAdjustment(60000));
+// console.log(finalAjustment());
+//
+//
+//
+// console.log(bonusPercentage);
+// let finalBonus = finalAjustment(bonusPercentage);
+// console.log(finalBonus);
+
+
+function salaryAdjustment(currentSalary) {
+  if (currentSalary > 65000) {
+    return -0.01;
+  } else {
+    return 0;
+  }
+}//end salaryAdjustment
+
+function lengthOfServiceBonus(employeeNumber) {
+  if (employeeNumber.length === 4 ) {
+    return 0.05;
+  }else {
+    return 0;
+  }
+}//end lengthOfServiceBonus
+
+// expecting a number representing a rating (0-5)
+function baseBonus(rating) {
+  console.log(rating);
+  if (rating<=2) {
+    return 0;
+  } else if (rating === 3) {
+    return 0.04;
+  } else if (rating === 4) {
+    return 0.06;
+  }else if (rating === 5) {
+    return 0.10
+  }
+}//end baseBonus
